ab245b37d82609b00797d7565b56392ff172b7f7
rafin <rafin@DESKTOP-3JQ3N4B>
2019-11-25 15:52:12 -0300

    Add project files.

---
+++ ControleEstoqueProduto.Web/Controllers/OperacaoController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControleEstoqueProduto.Web.Controllers
+{
+    public class OperacaoController : Controller
+    {
+        // GET: Operacao
+        public ActionResult EntradaProduto()
+        {
+            return View();
+        }
+        public ActionResult SaidaProduto()
+        {
+            return View();
+        }
+        public ActionResult LancPerdaProduto()
+        {
+            return View();
+        }
+        public ActionResult Inventario()
+        {
+            return View();
+        }
+    }
+}
